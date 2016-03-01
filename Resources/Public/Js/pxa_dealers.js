cb1904bcb3eebeaf7e95f61acfdf312f8524f64a
Alex <alex@pixelant.se>
2016-03-01 16:05:43 +0200

    Added cluster to map and lazy scrolling to the list view

--- Resources/Public/Js/pxa_dealers.js
+++ Resources/Public/Js/pxa_dealers.js
@@ -69,6 +69,7 @@ function initializeMapPxaDealers() {
           }            
   };
   var map = new google.maps.Map(document.getElementById("pxa-dealers-map"),mapOptions);
+ 
   //map.mapTypes.set('map_style', styledMap);
  // map.setMapTypeId('map_style');
   panorama = map.getStreetView();
@@ -93,6 +94,7 @@ function initializeMapPxaDealers() {
     map.fitBounds(bounds);
       map.setCenter(bounds.getCenter()); 
   }
+  var mc = new MarkerClusterer(map, markersArray);
 }
 function switchToStreetView(i) {
   var position = markersArray[i].getPosition();
@@ -178,4 +180,25 @@ $( document ).ready(function() {
 
     window.document.location = url;
   });
+
+  var numberOfRows = Math.floor(resultLimit/numberOfColumns);
+
+  for (i = 0; i <= numberOfRows; i++) { 
+      $('#' + i).fadeIn();
+  }
+
+  $(window).scroll(function() {
+  var lastRaw = $('.pxa-dealers-list').filter(":visible").last();
+  var lastRawId = $('.pxa-dealers-list').filter(":visible").last().attr('id');
+  var bottomOfDocWithOffset = ($(document).height() - $(window).height()) - ($(window).height()/3); //avoid offset().top because of css
+    if ($(window).scrollTop() >= bottomOfDocWithOffset) {
+      var tempRawId = parseInt(lastRawId, 10);
+      for (i = tempRawId + 1; i <= tempRawId + numberOfRows; i++) { 
+          $('#' + i).fadeIn();
+      }
+    }
+  });
+
+
 });
+
