9b2c487ba1aec00b13fb97582e086315d0bd4594
TimTa <timta@Resultifys-MacBook-Pro-2.local>
2018-05-03 12:22:05 +0200

    [BUGFIX] Move tca configuration, makeCategorizable to TCA/Overrides

--- Configuration/TCA/Overrides/tx_pxadealers_domain_model_dealer.php
+++ Configuration/TCA/Overrides/tx_pxadealers_domain_model_dealer.php
@@ -1,6 +1,17 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+call_user_func(
+    function () {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
+            'pxa_dealers',
+            'tx_pxadealers_domain_model_dealer'
+            // Do not use the default field name ("categories"), which is already used
+            // Also do not use a field name containing "categories" (see http://forum.typo3.org/index.php/t/199595/)
+        );
+    }
+);
+
 if (version_compare(TYPO3_version, '8.6', '>=')) {
     $columns = &$GLOBALS['TCA']['tx_pxadealers_domain_model_dealer']['columns'];
 
