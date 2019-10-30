7b33284a870738207e46c7b17f16c49a5411c638
Andriy Oprysko <pyvasyk@gmail.com>
2019-10-30 13:06:29 +0200

    [TASK] drop TYPO3 7

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.3.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
             'static_info_tables' => ''
         ],
         'conflicts' => [
